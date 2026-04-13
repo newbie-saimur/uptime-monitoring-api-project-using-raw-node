ddcae5afdfee6cf2a623c5a713c96ee4d122010d
newbie-saimur <saimurrahmanarnob@gmail.com>
2026-04-14 02:50:42 +0600

    Parsing Request

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Dependencies
 const http = require('http');
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 // App Object - Module Scaffolding
 const app = {};
@@ -19,7 +21,29 @@ app.createServer = () => {
 
 // Handle Request Response
 app.handleReqRes = (req, res) => {
-    res.end('Hello World!');
+    // Request Handling
+    const parsedUrl = url.parse(req.url, true); // True means allow query params for parsing
+    const { path } = parsedUrl;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const queryStringObject = parsedUrl.query;
+    const method = req.method.toLowerCase();
+    const headersObject = req.headers;
+
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('end', () => {
+        realData += decoder.end();
+        console.log(realData);
+        // Response Handling
+        res.end('Hello World!');
+    });
+
+    console.log(trimmedPath, method, queryStringObject, headersObject);
 };
 
 // Start the Server
